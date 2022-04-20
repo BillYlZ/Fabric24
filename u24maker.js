437180fb493087d647aa64af3ae0d1577a700e3c
byalniz <bilal.yalniz@yalone-it.de>
2022-04-20 14:13:35 +0200

    'a'

--- u24maker.js
+++ u24maker.js
@@ -2,7 +2,6 @@ const uCanvas = require("./src/uCanvas.js");
 const ePosition = require("./src/ePosition.js");
 const eHelper = require("./src/eHelper.js");
 const eCanvas = require("./src/eCanvas.js");
-const URL = require("~/repositories/repository.js");
 
 module.exports = class U24Maker {
   eCanvas = null;
